45aecea529fb9207301cf6edba64b3c4398c4cbd
Alok Ranjan <90891466+alokdevopss@users.noreply.github.com>
2022-11-02 18:53:54 +0530

    Update myfirstdag.py

--- myfirstdag.py
+++ myfirstdag.py
@@ -1,6 +1,7 @@
-from datetime import timedelta
+
 from airflow import DAG
-from airflow.operators.python_operators import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 
 default_args = {
